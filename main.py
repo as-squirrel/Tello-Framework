67a0203f3a9a0f05dcccb069e0ea171ed6333043
as-squirrel <114065413+as-squirrel@users.noreply.github.com>
2023-07-21 05:11:38 +0200

    Code Update

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, Response
+from djitellopy import Tello
+import threading
+import time
+import cv2
+
+app = Flask(__name__)
+tello = Tello()
+
+def video_feed():
+    while True:
+        frame = tello.get_frame_read().frame
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed_route():
+    return Response(video_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def drone_control():
+    while True:
+        if 'command' in request.form:
+            command = request.form['command']
+            if command == 'takeoff':
+                tello.takeoff()
+            elif command == 'land':
+                tello.land()
+            elif command == 'up':
+                tello.move_up(30)
+            elif command == 'down':
+                tello.move_down(30)
+            elif command == 'forward':
+                tello.move_forward(30)
+            elif command == 'backward':
+                tello.move_backward(30)
+            elif command == 'left':
+                tello.move_left(30)
+            elif command == 'right':
+                tello.move_right(30)
+            elif command == 'rotate_left':
+                tello.rotate_counter_clockwise(30)
+            elif command == 'rotate_right':
+                tello.rotate_clockwise(30)
+
+def start_drone_thread():
+    tello.connect()
+    tello.streamon()
+    time.sleep(2)
+    drone_thread = threading.Thread(target=drone_control)
+    drone_thread.start()
+
+if __name__ == '__main__':
+    start_drone_thread()
+    app.run(debug=True)
